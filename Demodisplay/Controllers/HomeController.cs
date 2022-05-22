e37a1f908c2f4cd672b78e19620248d67e92891e
Annie <annarohanbabu@gmail.com>
2022-05-22 08:07:31 +0530

    initial

---
+++ Demodisplay/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Demodisplay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demodisplay.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult About( User u)
+        {
+            try
+            {
+                ViewBag.Message = "Your application description page.";
+            }
+            catch(Exception e)
+            {
+
+            }
+
+            return RedirectToAction ("Contact");
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
